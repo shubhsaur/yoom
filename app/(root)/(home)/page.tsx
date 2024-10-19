79d27b2a39fdc8bffa0d71c0782b740003e075ac
shubhsaur <shubhamsaurabh@outlook.com>
2024-10-19 23:36:48 +0530

    made  upcoming meeting time dynamic at homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
 
 const Home = () => {
+
+  const { upcomingCalls } = useGetCalls();
+
+  const upcomingCallTime = upcomingCalls.map((call) => call?.state?.startsAt).sort((a, b) => (a as any) - (b as any))[0];
 
   const now = new Date();
   const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
@@ -9,7 +16,9 @@ const Home = () => {
     <section className='flex size-full flex-col gap-10 text-white'>
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
           <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-            <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meeting at: 12:30 PM</h2>
+            <h2 className='glassmorphism max-w-[420px] rounded py-2 text-center text-base font-normal'>
+              {upcomingCallTime ? `Upcoming Meeting at: ${upcomingCallTime?.toLocaleString([], {year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit'})}` : 'No Upcoming Meetings'}
+            </h2>
             <div className="flex flex-col gap-2">
               <h1 className='text-4xl font-extrabold lg:text-7xl'>
                 {time}
